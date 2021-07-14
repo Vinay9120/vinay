ad57d877bc51814c130cebc7a7cd69cb4b274cfa
Admin <Admin@LAPTOP-RUJO0KHN>
2021-07-13 21:13:05 -0700

    first Commit Change

---
+++ firstproject/src/firstproject/firstprog.java
@@ -0,0 +1,16 @@
+package firstproject;
+
+public class firstprog {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println("value ofc"+c);
+
+	}
+
+}
